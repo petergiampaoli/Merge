181d274bc4a91b7cb7ecf97f520f2fc01ef72898
peter giampaoli <petergiampaoli@gmail.com>
2018-09-17 19:55:51 -0400

    got the helper functions defined

--- bubblesort.js
+++ bubblesort.js
@@ -1,22 +1,45 @@
 // source code
 
-const tootsiepop = {};
-tootsiepop.lick = function () {
-  return 'licked';
-};
-tootsiepop.getToCenter = function () {
-  this.lick();
-  this.lick();
-  this.lick();
-  return 'got to center';
-};
+// const tootsiepop = {};
+// tootsiepop.lick = function () {
+//   return 'licked';
+// };
+// tootsiepop.getToCenter = function () {
+//   this.lick();
+//   this.lick();
+//   this.lick();
+//   return 'got to center';
+// };
+
+// const bubbleSort=(array)=>{
+//   swap();
+//   return array;
+// }
+
+// const swap =()=>{
+//   console.log(`swapped`);
+// }
 
 const bubbleSort=(array)=>{
-  swap();
-  return array;
+for (i = 0; i < array.length; i ++){
+  if (!valueChecker(array , i)){
+    swap(array, i);
+  }
+
+}
+return array;
 }
 
-const swap =()=>{
-  console.log(`swapped`);
+const swap =(array, index)=>{
+  const leftValue = array[index];
+  const rightValue = array[index +1];
+  array[index] = rightValue;
+  array[index+1] = leftValue;
 }
 
+const valueChecker =(array , index)=>{
+  if (array[index] === array.length -1){
+    return true;
+  }
+  return array[index] < array[index+1]
+}
